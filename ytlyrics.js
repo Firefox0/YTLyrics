2cfdf52cc0a6bc374e89e5985189523c99ab0dfe
Firefox0 <you@example.com>
2021-01-09 18:48:56 +0100

    refactoring

--- ytlyrics.js
+++ ytlyrics.js
@@ -1,3 +1,8 @@
+function get_youtube_title() {
+    let elements = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer");
+    return elements[0].firstChild.innerText;
+}
+
 function filter_title(title) {
     // Filter unnecessary parts of the title to increase
     // the chance of getting proper results.
@@ -12,16 +17,10 @@ function filter_title(title) {
     return title;
 }
 
-function get_youtube_title() {
-    let elements = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer");
-    return elements[0].firstChild.innerText;
-}
-
 function prepare_description() {
     // Create a section to load all the lyrics elements.
     let description_div = document.getElementById("description");
     let new_div = document.createElement("div");
-    new_div.id = "lyrics";
     description_div.insertAdjacentElement("afterend", new_div);
     return new_div;
 }
@@ -32,31 +31,28 @@ function delete_previous_lyrics() {
     lyrics_element.innerText = "";
 }
 
-function watching() {
+function watching_video() {
     // Check if user is watching a video.
     return window.location.href.includes("watch?v=");
 }
 
-async function click() {
-    // Use query from text input to update the description.
-    let new_query = input.value;
-    update(new_query);
+function watching_new_video() {
+    // Check if user is watching a new video.
+    if (watching_video()) {
+        let full_title = get_youtube_title();
+        if (previous_title == full_title) {
+            return;
+        }
+        previous_title = full_title;
+        let title = filter_title(full_title);
+        update_description(title);
+    }
 }
 
-function init() {
-    // Append elements to the description.
-    // Elements persist even when you are clicking on a new video.
-    // So instead of reloading the elements you can just manipulate them.
-    if (watching()) {
-        let description = prepare_description();
-        let elements = [
-            script_name, hint, input, button, seperator, song, source, lyrics_element
-        ];
-        for (i in elements) {
-            description.appendChild(elements[i]);
-        }
-        clearInterval(init_interval);
-    }
+async function submit() {
+    // Use query from text input to update the description.
+    let new_query = input.value;
+    update_description(new_query);
 }
 
 async function search_duckduckgo(query) {
@@ -82,7 +78,7 @@ async function search_duckduckgo(query) {
     return null;
 }
 
-async function update(title) {
+async function update_description(title) {
     // Update the description.
     delete_previous_lyrics();
     song.innerText = "Loading...\n";
@@ -104,45 +100,48 @@ async function update(title) {
     lyrics_element.innerText = lyrics;
 }
 
-async function main() {
-    // Check if user is watching a new video.
-    if (watching()) {
-        let full_title = get_youtube_title();
-        if (previous_title == full_title) {
-            return;
+function init() {
+    // Append elements to the description.
+    // Elements persist even when you are clicking on a new video.
+    // So instead of reloading the elements you can just manipulate them.
+    if (watching_video()) {
+        let description = prepare_description();
+        let elements = [
+            script_name, hint, input, button, seperator, song, source, lyrics_element
+        ];
+        for (i in elements) {
+            description.appendChild(elements[i]);
         }
-        previous_title = full_title;
-        let title = filter_title(full_title);
-        update(title);
+        clearInterval(init_interval);
     }
 }
 
 let previous_title = "";
 
-let script_name = document.createElement("h3")
+let script_name = document.createElement("h3");
 script_name.innerText = "\nYTLyrics";
 
 let source = document.createElement("a");
-let hint = document.createElement("span")
+let hint = document.createElement("span");
 hint.innerText = "Search:\n";
 
 let input = document.createElement("input");
 input.setAttribute("type", "input")
-input.style.width = "200px"
+input.style.width = "200px";
 input.style.height = "17px";
 
 let button = document.createElement("input");
 button.setAttribute("type", "button");
-button.setAttribute("value", "Submit")
-button.onclick = click
+button.setAttribute("value", "Submit");
+button.onclick = submit;
 button.style.width = "80px"
 button.style.height = "25px";
 
-let seperator = document.createElement("span")
+let seperator = document.createElement("span");
 seperator.innerText = "\n";
 
 let song = document.createElement("span");
 let lyrics_element = document.createElement("span");
 
-setInterval(main, 1000);
+setInterval(watching_new_video, 1000);
 let init_interval = setInterval(init, 1000);
